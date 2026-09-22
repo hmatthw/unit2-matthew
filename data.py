63ddcec70919b6abe9f5d857d68c8599367290ba
hmatthw <matthewh403@nycstudents.net>
2026-09-22 10:22:49 -0400

    rtsjtsrrjsthrt

--- data.py
+++ data.py
@@ -1,6 +1,16 @@
-def counting():
+
+
+""" def calculator():
+    bill = float(input("How much was the bill?"))
+    tip = int(input("How much would you like to tip? 0% / 15% / 20% / 25% (no % sign)"))
+    tip = tip / 100
+    total = bill * (1 + tip)
+    print(f"Your total is ${total}")
+calculator() """
+
+""" def counting():
     sentence = input("Type a sentence ")
     wordcount = sentence.split()
-    characters = len(sentence)
-    print("There are " + str(characters) + " letters in your sentence!")
-counting()
+    words = len(sentence)
+    print("There are " + str(words) + " words in your sentence!")
+counting() """
