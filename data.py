dd2b15c28da4991cb388f7bb021d89b254560c19
hmatthw <matthewh403@nycstudents.net>
2026-09-23 10:17:23 -0400

    9/23/26

--- data.py
+++ data.py
@@ -1,16 +1,22 @@
+""" def counting():
+    sentence = input("Type a sentence ")
+    wordcount = sentence.split()
+    words = len(wordcount)
+    print("There are " + str(words) + " words in your sentence!")
+counting() """
 
-
-""" def calculator():
+def calculator():
     bill = float(input("How much was the bill?"))
     tip = int(input("How much would you like to tip? 0% / 15% / 20% / 25% (no % sign)"))
     tip = tip / 100
     total = bill * (1 + tip)
     print(f"Your total is ${total}")
-calculator() """
+calculator()
 
-""" def counting():
-    sentence = input("Type a sentence ")
-    wordcount = sentence.split()
-    words = len(sentence)
-    print("There are " + str(words) + " words in your sentence!")
-counting() """
+#students = ["Natalie", "Martin", "Ben", "Stefania"]
+#students.append("Karas")
+#print(students.pop(0), students)
+#for student in students:
+#    if(student == "Ben"):
+#        print("Found Him!")
+
